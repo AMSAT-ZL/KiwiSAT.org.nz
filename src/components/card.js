c28e0cb5b186ce4a54723be289f2e0e99b57c3ba
Jim Tittsler <jimt@onjapan.net>
2019-01-03 10:57:06 +0900

    Sponsors cards

--- src/components/card.js
+++ src/components/card.js
@@ -10,13 +10,13 @@ const Card = props => (
       boxShadow: `0 10px 20px rgba(0,0,0,0.19), 0 6px 6px rgba(0,0,0,0.23)`,
     }}
   >
-    <a href={props.href}>
+    <a href={props.href} rel={props.rel}>
       <img
         src={props.src}
         alt={props.alt}
         width={props.width}
         height={props.height}
-        style={{ float: 'left' }}
+        style={{ float: 'left', marginRight: '1em' }}
       />
       {props.text}
       <br clear="all" />
@@ -27,10 +27,12 @@ const Card = props => (
 Card.defaultProps = {
   alt: '',
   text: '',
+  rel: '',
 };
 
 Card.propTypes = {
   href: PropTypes.string,
+  rel: PropTypes.string,
   src: PropTypes.string,
   alt: PropTypes.string,
   width: PropTypes.string,
--- src/pages/sponsors.js
+++ src/pages/sponsors.js
@@ -1,281 +1,190 @@
 import React from 'react';
 
 import Layout from '../components/layout';
-import Sponsor from '../components/sponsor';
+import Card from '../components/card';
 import SEO from '../components/seo';
+
+import masseyUni from '../images/sponsors/massey_uni.gif';
+import novozone from '../images/sponsors/novozone.gif';
+import tmd from '../images/sponsors/TMD_s.jpg';
+import austinGreen from '../images/sponsors/Catarama_logo.gif';
+import camsensor from '../images/sponsors/camsensor_logo.gif';
+import huberSuhner from '../images/sponsors/sponsor1.gif';
+import DTA from '../images/sponsors/DTA.jpg';
+import venco from '../images/sponsors/Venco_logo2.jpg';
+import camfilFarr from '../images/sponsors/Camfil_Farr.jpg';
+import tyco from '../images/sponsors/tyco.jpg';
+import atsb from '../images/sponsors/atsb.jpg';
+import fisherPaykel from '../images/sponsors/FP_logo.gif';
+import rakon from '../images/sponsors/rakon_sm.gif';
+import ksm from '../images/sponsors/HSM_LOGO.jpg';
+import DS from '../images/sponsors/DS_Logo_s.gif';
+import glueGuru from '../images/sponsors/glue_guru.jpg';
+import stanier from '../images/sponsors/Stanier_Logo.jpg';
+import johnsonAero from '../images/sponsors/JALogo.gif';
 
 const IndexPage = () => (
   <Layout>
-    <SEO title="Sponsors" keywords={[`KiwiSAT`, `New Zealand`]} />
+    <SEO text="Sponsors" keywords={[`KiwiSAT`, `New Zealand`]} />
     <h2>
       AMSAT-ZL and the KiwiSAT Team are grateful for the willing support of:
     </h2>
-    <Sponsor
-      url="http://www.massey.ac.nz/"
+    <Card
+      href="http://www.massey.ac.nz/"
       text="Support for Space-frame Technology, Space Environmental Testing and Battery Analysis."
-      src="images/sponsors/massey_uni.gif"
+      src={masseyUni}
+      width="170"
+      height="92"
     />
-    <div class="sponsor">
-      <a
-        href="http://www.massey.ac.nz/"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        <img
-          src="images/sponsors/massey_uni.gif"
-          alt="Support for Space-frame Technology, Space Environmental Testing and Battery Analysis."
-          title="Support for Space-frame Technology,  Space Environmental Testing and Battery Analysis."
-          width="170"
-          height="92"
-        />
-      </a>
-    </div>
+    <Card
+      href="http://www.novozone.co.nz/"
+      src={novozone}
+      alt=""
+      text="Electronic circuit design and construction assistance by Novozone Ltd. specialists in ozone applications for air and water purification."
+      width="170"
+      height="92"
+    />
 
-    <div class="sponsor">
-      <a
-        href="http://www.novozone.co.nz/"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        <img
-          src="images/sponsors/novozone.gif"
-          alt="Electronic circuit design and construction assistance by Novozone Ltd. specialists in ozone applications for air and water purification."
-          title="Electronic circuit design and construction assistance by Novozone Ltd. specialists in ozone applications for air and water purification."
-          width="170"
-          height="92"
-        />
-      </a>
-    </div>
+    <Card
+      href="http://tmd.co.nz/index.htm"
+      src={tmd}
+      alt=""
+      text="Prototype design development, assembly and testing of KiwiSAT's U/V transponder."
+      width="170"
+      height="92"
+    />
 
-    <div class="sponsor">
-      <a
-        href="http://tmd.co.nz/index.htm"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        <img
-          src="images/sponsors/TMD_s.jpg"
-          alt="Prototype design development, assembly and testing of KiwiSAT's U/V transponder."
-          title="Prototype design development, assembly and testing of KiwiSAT's U/V transponder."
-          width="170"
-          height="92"
-        />
-      </a>
-    </div>
-
-    <div class="sponsor">
-      <img
-        src="images/sponsors/Catarama_logo.gif"
-        alt="Austin Green (Proprietor) - Leading operating system/bootloader and 
+    <Card
+      src={austinGreen}
+      alt=""
+      text="Austin Green (Proprietor) - Leading operating system/bootloader and 
     ADAC software development."
-        title="Austin Green (Proprietor) - Leading operating system/bootloader and 
-    ADAC software development."
-        width="170"
-        height="92"
-      />
-    </div>
+      width="170"
+      height="92"
+    />
 
-    <div class="sponsor">
-      <a
-        href="http://www.camsensor.com/"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        <img
-          src="images/sponsors/camsensor%20logo.gif"
-          alt="Horizon camera design/construction and assistance with general satellite housekeeping software development."
-          title="Horizon camera design/construction and assistance with general satellite housekeeping software development."
-          width="170"
-          height="92"
-        />
-      </a>
-    </div>
+    <Card
+      href="http://www.camsensor.com/"
+      src={camsensor}
+      alt=""
+      text="Horizon camera design/construction and assistance with general satellite housekeeping software development."
+      width="170"
+      height="92"
+    />
 
-    <div class="sponsor">
-      <a
-        href="http://www.hubersuhner.com/"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        <img
-          src="images/sponsors/sponsor1.gif"
-          alt="RF cables and connectors donated by InTELCOM SERVICES LTD. and Huber &amp; Suhner (Australia) PTY LTD"
-          title="RF cables and connectors donated by InTELCOM SERVICES LTD. and Huber &amp; Suhner (Australia) PTY LTD"
-          width="170"
-          height="92"
-        />
-      </a>
-    </div>
+    <Card
+      href="http://www.hubersuhner.com/"
+      src={huberSuhner}
+      alt=""
+      text="RF cables and connectors donated by InTELCOM SERVICES LTD. and Huber &amp; Suhner (Australia) PTY LTD"
+      width="170"
+      height="92"
+    />
 
-    <div class="sponsor">
-      <img
-        src="images/sponsors/DTA.jpg"
-        alt="Shake table for structural integrity testing"
-        title="Shake table for structural integrity testing"
-        width="170"
-        height="92"
-      />
-    </div>
+    <Card
+      src={DTA}
+      alt=""
+      text="Shake table for structural integrity testing"
+      width="170"
+      height="92"
+    />
 
-    <div class="sponsor">
-      <a
-        href="http://www.vencon.com/"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        <img
-          src="images/sponsors/Venco_logo2.jpg"
-          alt="Battery analysis equipment and software"
-          title="Battery analysis equipment and software"
-          width="170"
-          height="92"
-        />
-      </a>
-    </div>
+    <Card
+      href="http://www.vencon.com/"
+      src={venco}
+      alt=""
+      text="Battery analysis equipment and software"
+      width="170"
+      height="92"
+    />
 
-    <div class="sponsor">
-      <a
-        href="http://www.camfilfarr.com/"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        <img
-          src="images/sponsors/Camfil Farr.jpg"
-          alt="Spacecraft Ground Support Equipment (Clean-Box) air filtration design assistance"
-          title="Spacecraft Ground Support Equipment (Clean-Box) air filtration design assistance."
-          width="170"
-          height="92"
-        />
-      </a>
-    </div>
+    <Card
+      href="http://www.camfilfarr.com/"
+      src={camfilFarr}
+      alt=""
+      text="Spacecraft Ground Support Equipment (Clean-Box) air filtration design assistance."
+      width="170"
+      height="92"
+    />
 
-    <div class="sponsor">
-      <a
-        href="http://www.tycoelectronics.co.nz/"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        <img
-          src="images/sponsors/tyco.jpg"
-          alt="Tyco Electronics is proud to support the Kiwi-Sat project with the supply of space qualified heat-shrink material."
-          title="Tyco Electronics is proud to support the Kiwi-Sat project with the supply of space qualified heat-shrink material."
-          width="170"
-          height="92"
-        />
-      </a>
-    </div>
+    <Card
+      href="http://www.tycoelectronics.co.nz/"
+      src={tyco}
+      alt=""
+      text="Tyco Electronics is proud to support the Kiwi-Sat project with the supply of space qualified heat-shrink material."
+      width="170"
+      height="92"
+    />
 
-    <div class="sponsor">
-      <div align="center">
-        <a href="http://www.atsb.my" target="_blank" rel="noopener noreferrer">
-          <img
-            src="images/sponsors/atsb.jpg"
-            alt="ASTRONAUTIC TECHNOLOGY(M) SDN. BHD. For location and provision of small quantity space quality RF components in support of the U/V transponder"
-            title="ASTRONAUTIC TECHNOLOGY(M) SDN. BHD. For location and provision of small quantity space quality RF components in support of the U/V transponder"
-            width="170"
-            height="92"
-          />
-        </a>
-      </div>
-    </div>
+    <Card
+      href="http://www.atsb.my"
+      src={atsb}
+      alt=""
+      text="ASTRONAUTIC TECHNOLOGY(M) SDN. BHD. For location and provision of small quantity space quality RF components in support of the U/V transponder"
+      width="170"
+      height="92"
+    />
 
-    <div class="sponsor">
-      <a
-        href="http://www.fphcare.com/"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        <img
-          src="images/sponsors/FP_logo.GIF"
-          alt="Fisher &amp; Paykel Healthcare for Facilities and expertise for structure and component vibration testing."
-          title="Fisher &amp; Paykel Healthcare for Facilities and expertise for structure and component vibration testing."
-          width="170"
-          height="92"
-        />
-      </a>
-    </div>
+    <Card
+      href="http://www.fphcare.com/"
+      src={fisherPaykel}
+      alt=""
+      text="Fisher &amp; Paykel Healthcare for Facilities and expertise for structure and component vibration testing."
+      width="170"
+      height="92"
+    />
 
-    <div class="sponsor">
-      <a href="http://www.rakon.com/" target="_blank" rel="noopener noreferrer">
-        <img
-          src="images/sponsors/rakon_sm.gif"
-          alt="Rakon Limited is a world leader in the development of high performance frequency control technology based on quartz crystals, which lie at the heart of all electronic products."
-          title="Rakon Limited is a world leader in the development of high performance frequency control technology based on quartz crystals, which lie at the heart of all electronic products."
-          width="170"
-          height="92"
-        />
-      </a>
-    </div>
+    <Card
+      href="http://www.rakon.com/"
+      src={rakon}
+      alt=""
+      text="Rakon Limited is a world leader in the development of high performance frequency control technology based on quartz crystals, which lie at the heart of all electronic products."
+      width="170"
+      height="92"
+    />
 
-    <div class="sponsor">
-      <a
-        href="http://www.hsmicrowave.com/"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        <img
-          src="images/sponsors/HSM_LOGO.jpg"
-          alt="Assembly tooling and space materials aquisition/assistance."
-          title="Assembly tooling and space materials aquisition/assistance"
-          width="170"
-          height="92"
-        />
-      </a>
-    </div>
+    <Card
+      href="http://www.hsmicrowave.com/"
+      src={ksm}
+      alt=""
+      text="Assembly tooling and space materials aquisition/assistance"
+      width="170"
+      height="92"
+    />
 
-    <div class="sponsor">
-      <a
-        href="http://www.designsolutions.co.nz/"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        <img
-          src="images/sponsors/DS_Logo_s.GIF"
-          alt="For graphics in support of the ADAC telemetry download and processing software."
-          title="For graphics in support of the ADAC telemetry download and processing software."
-          width="170"
-          height="92"
-        />
-      </a>
-    </div>
+    <Card
+      href="http://www.designsolutions.co.nz/"
+      src={DS}
+      alt=""
+      text="For graphics in support of the ADAC telemetry download and processing software."
+      width="170"
+      height="92"
+    />
 
-    <div class="sponsor">
-      <a
-        href="http://www.glueguru.co.nz/index.php"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        <img
-          src="images/sponsors/glue_guru.jpg"
-          alt="Glue Guru Industrial supporting KiwiSAT with specialised structural compounds/adhesives"
-          title="Glue Guru Industrial supporting KiwiSAT with specialised structural compounds/adhesives"
-          width="170"
-          height="92"
-        />
-      </a>
-    </div>
+    <Card
+      href="http://www.glueguru.co.nz/index.php"
+      src={glueGuru}
+      alt=""
+      text="Glue Guru Industrial supporting KiwiSAT with specialised structural compounds/adhesives"
+      width="170"
+      height="92"
+    />
 
-    <div class="sponsor">
-      <img
-        src="images/sponsors/Stanier Logo.jpg"
-        alt="Specialist in Precision, Production and Model Engineering - manufacturer of KiwiSAT's space-frame."
-        title="Specialist in Precision, Production and Model Engineering - manufacturer of KiwiSAT's space-frame."
-        width="170"
-        height="92"
-      />
-    </div>
-
-    <div class="sponsor">
-      <a href="http://johnson.aero/" target="_blank" rel="noopener noreferrer">
-        <img
-          src="images/sponsors/JALogo.gif"
-          alt="Johnson Aero - aeronautics and astronautics projects - from sea to shining star."
-          title="Johnson Aero - aeronautics and astronautics projects - from sea to shining star."
-          width="170"
-          height="92"
-        />
-      </a>
-    </div>
+    <Card
+      src={stanier}
+      alt=""
+      text="Specialist in Precision, Production and Model Engineering - manufacturer of KiwiSAT's space-frame."
+      width="170"
+      height="92"
+    />
+    <Card
+      href="http://johnson.aero/"
+      src={johnsonAero}
+      alt=""
+      text="Johnson Aero - aeronautics and astronautics projects - from sea to shining star."
+      width="170"
+      height="92"
+    />
   </Layout>
 );
 
