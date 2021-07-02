8ac9273c27df907ab9ea0dcb4b1aa333fa46d248
Jim Tittsler <jimt@onjapan.net>
2021-07-02 13:37:58 +0900

    narrow text area on wide screens

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,13 @@ module.exports = {
   theme: {
     container: {
       center: true,
+      screens: {
+        sm: '640px',
+        md: '768px',
+        lg: '1024px',
+        xl: '1024px',     // 1280px
+        '2xl': '1024px',  // 1536px
+      },
     },
     extend: {
       colors: {},
